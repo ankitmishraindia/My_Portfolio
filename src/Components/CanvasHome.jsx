08679bbc0ab800b595c7386aea461c62c903d6f6
ankit mishra <mishraankit987@gmail.com>
2024-04-27 14:00:23 +0530

    changed animation to simly moving circle particle

--- src/Components/CanvasHome.jsx
+++ src/Components/CanvasHome.jsx
@@ -6,107 +6,61 @@ function CanvasHome(){
 useEffect(()=>{
     const canvas=document.getElementById('canvas1') 
     const ctx=canvas.getContext('2d')
-    const particlesArray=[]
+     console.log(ctx)
     
     canvas.width=window.innerWidth;
     canvas.height=window.innerHeight;
-
-    window.addEventListener('resize',function(){
-        canvas.width=window.innerWidth;
-    canvas.height=window.innerHeight;
-    })
-    
-    const mouse={
-        x:undefined,
-        y:undefined
-    }
-
-    canvas.addEventListener('mousemove',function(event){
-        mouse.x=event.x;
-        mouse.y=event.y;
-    })
-
     class Particle{
-        constructor(){
-            this.x=Math.random() * canvas.width;
-            this.y=Math.random() * canvas.height;
-            this.size=Math.random()*15 +1;
-            this.baseSpeedX=(Math.random() *3 -2.5)*0.1;
-            this.baseSpeedY=(Math.random() *3 -2.5)*0.1;
-            this.speedX=this.baseSpeedX;
-            this.speedY=this.baseSpeedY;
-            this.color=`rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`;
-            this.opacity=''
-        }
-        update(){
-           this.x +=this.speedX;
-           this.y +=this.speedY;
-           //check boundry collision
-           if(this.x+this.size>canvas.width||this.x - this.size < 0){
-            this.speedX=-this.speedX;
+           constructor(effect){
+            this.effect=effect;
+            this.x=Math.random()* this.effect.width;
+            this.y=Math.random() * this.effect.height;
+            this.radius=2;
+            this.speedX=1;
            }
-           if(this.y + this.size >canvas.height || this.y - this.size <0){
-            this.speedY=-this.speedY;
+           draw(context){
+            context.fillStyle='hsl('+this.x+',100%,50%)';
+            context.beginPath();
+            context.arc(this.x,this.y,this.radius,0,Math.PI*2)
+            context.fill()
            }
-          
-           
-    }
-        draw(){
-            //change opacity during hover
-            
-            const differnceX=mouse.x-this.x;
-            const differnceY=mouse.y-this.y;
-            const distance=Math.sqrt(differnceX**2+differnceY**2)
-            if(distance<100){
-               this.opacity=1;
-            }else{
-              this.opacity=0.4
-            }
-           ctx.fillStyle=this.color;
-           ctx.strokeStyle='white';
-           ctx.lineWidht='5px';
-           ctx.globalAlpha=this.opacity;
-           ctx.beginPath();
-            ctx.arc(this.x,this.y,this.size,3,Math.PI*2)
-          
-           
-           ctx.fill();
-           ctx.stroke()
-        }
-        
+           update(){
+            this.x +=this.speedX;
+            if(this.x>this.effect.width||this.x<0) this.speedX *=-1
+           }
     }
 
-    function init(){
-          for(let i=0;i<100;i++){
-            particlesArray.push(new Particle())
-          }
-    }
-    init()
-    
-    // draw and give speed to each particle
-    function handleParticle(){
-        for(let i=0;i<particlesArray.length;i++){
-            particlesArray[i].update();
-            particlesArray[i].draw();
-            
-            // remove very small particles
-            if(particlesArray[i].size<=0.7){
-                particlesArray.splice(i,1)
-                console.log(particlesArray.length)
-                i--;
-                
-            }
+
+    class Effect{
+       constructor(canvas){
+        this.canvas=canvas;
+        this.height=this.canvas.height;
+        this.width=this.canvas.width;
+        this.particles=[];
+        this.numberOfParticles=100;
+        this.createParticles()
+       }
+       createParticles(){
+        for(let i=0;i<this.numberOfParticles;i++){
+            this.particles.push(new Particle(this))
         }
+       }
+       handleParticles(context){
+        this.particles.forEach(particle=>{
+            particle.draw(context)
+            particle.update()
+        })
+       }
     }
-     
-    //animation
+    const effect=new Effect(canvas)
+    
+
     function animate(){
         ctx.clearRect(0,0,canvas.width,canvas.height)
-        handleParticle()
+        effect.handleParticles(ctx)
         requestAnimationFrame(animate)
     }
-    animate()
-   
+   animate()
     
 },[])
 
--- src/Sections/HomeSection.jsx
+++ src/Sections/HomeSection.jsx
@@ -21,21 +21,21 @@ function HomeSection(){
 
     return(
         <div id="home" className="h-screen w-full flex items-center justify-center relative ">
-                    <div className="flex flex-col items-center justify-center gap-4 md:gap-10 lg:gap-14 text-center z-10">
-                          <h1 className="text-4xl sacramento-regular md:text-6xl text-white bg-transparent  tracking-wide flex flex-col items-center lg:items-stretch">
+                    <div className="flex flex-col items-center justify-center gap-4 md:gap-10 lg:gap-10 text-center z-10">
+                          <h1 className="text-4xl md:text-5xl text-white bg-transparent ">
                             
-                            <p className="font-extrabold">
-                              Namaskaram, I&apos;m <span className="text-yellow-500">
+                            <p className="">
+                              Namaskaram, I&apos;m <span className="text-red-500">
                                  Ankit Mishra,
                                 </span>
                                 <br />
-                                <span className="text-red-500">Full stack Web developer.</span>
+                                <span className="">Full stack Web developer.</span>
                                 </p>
                             
                           </h1>
                          
                          <a  onClick={()=>scrollToSection('projects')}>
-                                <button className="px-7 flex items-center gap-2 bg-transparent lg:px-10 py-2 lg:py-3 font-bold  hover:bg-yellow-600 text-white border-2 border-white hover:text-black">
+                                <button className="px-7 flex items-center gap-2 bg-transparent py-2  font-bold  hover:bg-yellow-600 text-white border-2 border-white hover:text-black">
                                   PROJECTS
                                   <BiSolidBriefcase/>
                                   </button>
